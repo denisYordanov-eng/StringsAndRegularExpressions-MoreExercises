a4185da243376c43eba1ad2d28bf40dc8e7278c5
denisYordanov-eng <denisivov437@gmail.com>
2025-06-11 04:17:05 +0300

    Made a task.

---
+++ 01-Censorship/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_Censorship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text =  Console.ReadLine();
+            string replacePatern = $@"{Regex.Escape(word)}";
+
+            string newText = Regex.Replace
+                (text, replacePatern, new string('*', word.Length));
+
+            Console.WriteLine (newText);
+        }
+    }
+}
