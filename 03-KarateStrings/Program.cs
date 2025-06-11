72ec758bf7f03305ca29065e13c8cf121c8ac187
denisYordanov-eng <denisivov437@gmail.com>
2025-06-11 06:08:46 +0300

    Made a task.

---
+++ 03-KarateStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_KarateStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            int power = 0;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == '>')
+                {
+                    power += int.Parse(path[i + 1].ToString());
+                    i++;
+                    while (power > 0 && i < path.Length)
+                    {
+                        if (path[i] == '>')
+                        {
+                            break;
+                        }
+                      path =  path.Remove(i, 1);
+                        power--;
+                    }
+                    i--;
+                }
+            }
+            Console.WriteLine(path);
+        }
+    }
+}
