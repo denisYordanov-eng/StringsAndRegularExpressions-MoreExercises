0e13b9859049a6d1258a639dcac7d27bb0a6cc38
denisYordanov-eng <denisivov437@gmail.com>
2025-06-11 13:52:10 +0300

    Made a task.

---
+++ 04-MorseCodeUpgraded/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04_MorseCodeUpgraded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            
+        StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                string currentNum = (input[i]);
+              int sum = CheckingEveryDigit(currentNum);
+                sum += CheckForSequence(currentNum);
+                char letter = (char)sum;
+                sb.Append(letter.ToString());
+            }  
+            Console.WriteLine(sb.ToString());   
+        }
+
+        private static int CheckForSequence(string currentNum)
+        {
+            int sum = 0;
+            int count = 1;
+            for (int i = 1; i < currentNum.Length; i++)
+            {
+                char currentDigit = currentNum[i];
+                char previousDigit = currentNum[i - 1];
+                if (currentDigit == previousDigit)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 1)
+                    {
+                        sum += count;
+                    }
+                    count = 1;
+                }
+            }
+            if (count > 1)
+            {
+                sum += count;
+            }
+            return sum;
+        }
+
+        private static int CheckingEveryDigit(string currentNum)
+        {
+            int sum = 0;
+            foreach (char digit in currentNum)
+            {
+                
+                if (digit == '0')
+                {
+                    sum += 3;
+                }
+                else if (digit == '1') 
+                {
+                    sum += 5;
+                }
+                
+            }
+            return sum;
+        }
+    }
+}
---
+++ 04-MorseCodeUpgraded/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("04-MorseCodeUpgraded")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04-MorseCodeUpgraded")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a628a87b-6e02-4c1f-8574-98caabf5cd5d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
